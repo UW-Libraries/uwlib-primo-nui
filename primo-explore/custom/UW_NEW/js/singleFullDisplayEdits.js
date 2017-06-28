9a7ba06866b3e21ddc38fb9852ce24474818c889
Kate Deibel <deibel@uw.edu>
2017-06-28 14:52:42 -0700

    Working code

--- primo-explore/custom/UW_NEW/js/singleFullDisplayEdits.js
+++ primo-explore/custom/UW_NEW/js/singleFullDisplayEdits.js
@@ -5,27 +5,63 @@ class GenericSFDEController {
     constructor($scope, $element) {
         this._$scope = $scope;
         this._$elem = $element;
-        this.moveNoteHBR = false;
-        this.almaMashups = $element.parent()[0].querySelector('iframe');
+        this.addNoteHBR = false;
+        //this._$scope.almaMashups = this._$elem.parent()[0].querySelector('iframe');
     }
     $onInit() {
-        this.moveNoteHBR = this.isHarvardBusinessReviewItem();
-        console.log(this._$elem.parent()[0]);
-        console.log(this.parentCtrl);
-        console.log(this.parentCtrl.isAlmaGetit() === false)
+        this.addNoteHBR = this.isHarvardBusinessReviewItem();
     }
     $postLink() {
         
-        
+        var elem = this._$elem;
         console.log('POSTLINK');
-        if(this.moveNoteHBR) {
-            this._$scope.$watch('$ctrl.almaMashups', function() { console.log('MASH'); } );
-            // templateUrl: '/primo-explore/custom/UW_NEW/html/fullPageOptionalNotes.html'
-            //moveNote('localNoteHBR');
+        if(this.addNoteHBR) {
+            this._$scope.$watch(
+               function() { return elem.parent()[0].querySelector('prm-alma-mashup'); },
+               function(mashupElem, nullVal) {
+                  if(mashupElem !== null) {
+                     // we can move the note
+                     angular.element(mashupElem).prepend(document.getElementById('localNoteHBR'));
+                  }
+               
+               } 
+            );
+
 
         }
     }
     
+
+    
+    
+    moveNote(noteID) {
+        console.log('MOVE');
+        var note = this._$elem[0].querySelector('#'+noteID);
+        console.log(note);
+        this.almaMashups[0].prepend(note);
+        
+    }
+    isHarvardBusinessReviewItem() {
+      if(this.parentCtrl.index != 1) // is not the view it online tab
+         return;
+      var issn = this.parentCtrl.item.pnx.search.issn;
+      if(issn === undefined)
+         return false;
+
+      var hbr_regex = /^0017-?8012$/g;    
+      if( Object.prototype.toString.call( issn ) === '[object Array]' ) {
+         for(var i=0; i<issn.length; i++) {
+            if( hbr_regex.test(String(issn[i])) )
+               return true;
+            }
+         return false;    
+      }
+      if( typeof issn === 'string') {
+         return hbr_regex.test(issn);
+      }
+      return false;
+   };
+   
     fixAlmaSkin() {
         var OLD_SKIN = 'uw_sandbox_skin';
         var NEW_SKIN = 'uw_new_sandbox_skin';
@@ -44,37 +80,11 @@ class GenericSFDEController {
         }        
 
     }
-    
-    
-    moveNote(noteID) {
-        console.log('MOVE');
-        var note = this._$elem[0].querySelector('#'+noteID);
-        console.log(note);
-        this.almaMashups[0].prepend(note);
-        
-    }
-    isHarvardBusinessReviewItem() {
-      var issn = this.parentCtrl.item.pnx.search.issn;
-      if(issn === undefined)
-         return false;
-
-      var hbr_regex = /^0017-?8012$/g;    
-      if( Object.prototype.toString.call( issn ) === '[object Array]' ) {
-         for(var i=0; i<issn.length; i++) {
-            if( hbr_regex.test(String(issn[i])) )
-               return true;
-            }
-         return false;    
-      }
-      if( typeof issn === 'string') {
-         return hbr_regex.test(issn);
-      }
-      return false;
-   };
 }
 
 app.component('prmFullViewServiceContainerAfter', {
     bindings: {parentCtrl: '<'}, /*bind to parentCtrl to read PNX*/
-    controller: 'genericSFDEController'
+    controller: 'genericSFDEController',
+    templateUrl: '/primo-explore/custom/UW_NEW/html/fullPageOptionalNotes.html'
 }).controller('genericSFDEController',GenericSFDEController)
 
