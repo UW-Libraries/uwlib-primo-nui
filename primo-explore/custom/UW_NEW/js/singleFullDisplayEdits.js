98147c54f3e12014d9001b030f323dc9d635525e
Kate Deibel <deibel@uw.edu>
2017-06-28 19:50:29 -0700

    working local notes/alma fix

--- primo-explore/custom/UW_NEW/js/singleFullDisplayEdits.js
+++ primo-explore/custom/UW_NEW/js/singleFullDisplayEdits.js
@@ -2,52 +2,84 @@ let app = angular.module('singleFullDisplayEdits',[]);
 export default app;
 
 class GenericSFDEController {
-    constructor($scope, $element) {
-        this._$scope = $scope;
-        this._$elem = $element;
-        this.addNoteHBR = false;
-        //this._$scope.almaMashups = this._$elem.parent()[0].querySelector('iframe');
-    }
-    $onInit() {
-        this.addNoteHBR = this.isHarvardBusinessReviewItem();
-    }
-    $postLink() {
-        
-        var elem = this._$elem;
-        console.log('POSTLINK');
-        if(this.addNoteHBR) {
-            this._$scope.$watch(
-               function() { return elem.parent()[0].querySelector('prm-alma-mashup'); },
-               function(mashupElem, nullVal) {
-                  if(mashupElem !== null) {
-                     // we can move the note
-                     angular.element(mashupElem).prepend(document.getElementById('localNoteHBR'));
-                  }
-               
-               } 
-            );
-
-
-        }
-    }
-    
-
-    
-    
-    moveNote(noteID) {
-        console.log('MOVE');
-        var note = this._$elem[0].querySelector('#'+noteID);
-        console.log(note);
-        this.almaMashups[0].prepend(note);
-        
-    }
-    isHarvardBusinessReviewItem() {
+   constructor($scope, $element) {
+      this._$scope = $scope;
+      this._$elem = $element;
+      
+      // Local Notes Handling 
+      this.localNoteOrder = ['HBR']; // place in order you want them to appear ultimately
+      this.localNoteStatus = []; // associative boolean array of notes to add
+      this.localNotesPresent = false;
+      // End Local Notes Handling
+   }
+   $onInit() {
+      // Local Notes Handling 
+      this.localNoteStatus['HBR'] = this.addHarvardBusinessReviewNote();
+      // OR the localNoteStatus together
+      var $localStatus = this.localNoteStatus;
+      this.localNotesPresent = Object.keys(this.localNoteStatus).reduce( 
+         function(run,k) { 
+            return (run || $localStatus[k]);
+         },
+         false
+      );
+      // End Local Notes Handling 
+   }
+   $postLink() {    
+      var $localScope = this._$scope;
+      var $localElem = this._$elem;
+      
+      // IFrame Fixing
+      this._$scope.$watch(
+         function() { 
+            return $localElem.parent()[0].querySelector('iframe'); 
+         },
+         function(iframe, nullVal) {
+            if(iframe !== null) {
+               var OLD_SKIN = 'uw_sandbox_skin';
+               var NEW_SKIN = 'uw_new_sandbox_skin';
+               var src = iframe.getAttributeNode('src').value;        
+               if( src.indexOf('req.skin='+OLD_SKIN) != -1 ) {               
+                  src = src.replace('req.skin='+OLD_SKIN, 'req.skin='+NEW_SKIN);
+                  iframe.getAttributeNode('src').value = src;
+               }  
+            }
+         }
+      );
+      
+      // Local Notes Handling 
+      var $localOrder = this.localNoteOrder;
+      var $localStatus = this.localNoteStatus;      
+      // Determine if we need to move some local notes before the Alma iframes
+      if(this.localNotesPresent) {
+         this._$scope.$watch(
+            function() { 
+               return $localElem.parent()[0].querySelector('prm-alma-mashup'); 
+            },
+            function(mashupElem, nullVal) {
+               if(mashupElem !== null) {
+                  // we can move the notes
+                  $localOrder.reverse().forEach(
+                     function(note) {
+                        if($localStatus[note]) {
+                           var noteClass = '.localNote' + note;
+                           angular.element(mashupElem).prepend($localElem[0].querySelector(noteClass));
+                        }
+                     }
+                  );
+               }
+            } 
+         );
+      }
+      // End Local Notes Handling
+   }
+   
+   addHarvardBusinessReviewNote() {
       if(this.parentCtrl.index != 1) // is not the view it online tab
-         return;
+         return false;
       var issn = this.parentCtrl.item.pnx.search.issn;
       if(issn === undefined)
          return false;
-
       var hbr_regex = /^0017-?8012$/g;    
       if( Object.prototype.toString.call( issn ) === '[object Array]' ) {
          for(var i=0; i<issn.length; i++) {
@@ -60,26 +92,7 @@ class GenericSFDEController {
          return hbr_regex.test(issn);
       }
       return false;
-   };
-   
-    fixAlmaSkin() {
-        var OLD_SKIN = 'uw_sandbox_skin';
-        var NEW_SKIN = 'uw_new_sandbox_skin';
-        
-        
-        var iframe = this._$elem.parent()[0].querySelector('iframe');
-        console.log(iframe);
-        if(iframe === undefined || iframe === null)
-            return;
-        console.log('iframe found');
-        var src = iframe.getAttributeNode('src').value;        
-        if( src.indexOf('req.skin='+OLD_SKIN) != -1 ) {               
-            src = src.replace('req.skin='+OLD_SKIN, 'req.skin='+NEW_SKIN);
-            iframe.getAttributeNode('src').value = src;
-            iframe.getAttributeNode('ng-src').value = src;
-        }        
-
-    }
+   };  
 }
 
 app.component('prmFullViewServiceContainerAfter', {
