4c4482b379019212c8d5fc6a56f19ac0d72e50c2
Steven Pryor <31045615+troub1@users.noreply.github.com>
2017-12-29 11:03:43 -0800

    Update production UW skin with 'atoz' fixes (journal search bar)

--- gulp/config.js
+++ gulp/config.js
@@ -101,8 +101,8 @@ var SERVERS = {
 };
 
 /*Note that for SSL environments (https) define the server as: var PROXY_SERVER = https://your-server:443*/
-var PROXY_SERVER = 'https://alliance-primo-sb.hosted.exlibrisgroup.com:443';
-// var PROXY_SERVER = 'https://alliance-primo.hosted.exlibrisgroup.com:443';
+// var PROXY_SERVER = 'https://alliance-primo-sb.hosted.exlibrisgroup.com:443';
+var PROXY_SERVER = 'https://alliance-primo.hosted.exlibrisgroup.com:443';
 
 let buildParams = {
     customFile: customFile,
